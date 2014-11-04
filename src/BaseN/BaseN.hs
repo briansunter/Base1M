8f4767f93ce1a17ac467ed1b47feae2be5d0c605
Brian Sunter <bsunter@gmail.com>
2014-11-03 16:42:52 -0800

    feat(BaseN): Basically Works

--- src/BaseN/BaseN.hs
+++ src/BaseN/BaseN.hs
@@ -6,37 +6,28 @@
 
 module BaseN 
 (BaseN) where
-
-import Data.List(elemIndex)
-import Data.Maybe(fromJust)
+import Data.List(elemIndex,all,genericLength)
+import Data.Maybe(fromJust,isJust)
 
 base10CharSet = ['0'..'9']
 
-data BaseN = BaseN [Char] [Char] deriving(Show)
+data BaseN = BaseN [Char] Integer deriving(Show)
 instance Num BaseN where
-  (BaseN c x) + (BaseN c' y)  = BaseN c $ encodeBase c $ ((decodeBase c x) + (decodeBase c' y))
-  (BaseN c x) * (BaseN c' y)  = BaseN c $ encodeBase c $ ((decodeBase c x) * (decodeBase c' y))
-  abs (BaseN c x)  = BaseN c $ encodeBase c $ abs $ (decodeBase c x) 
-  signum (BaseN c x) = BaseN c $ encodeBase c $  signum $ decodeBase c x
-  fromInteger i  =  BaseN base10CharSet $ encodeBase base10CharSet i 
-  negate (BaseN c x) = BaseN c $ encodeBase c $ negate $ decodeBase c x
+  (BaseN c x) + (BaseN c' y)  = (BaseN c (x + y)) 
+  (BaseN c x) * (BaseN c' y)  = (BaseN c (x * y)) 
+  abs (BaseN c x)  = BaseN c (abs x)
+  signum (BaseN c x) = BaseN c (signum x)
+  fromInteger i  =  BaseN base10CharSet i 
+  negate (BaseN c x) = BaseN c (negate x) 
 
-numToDigs :: Integer -> Integer -> [Integer]
-numToDigs 0 _ = []
-numToDigs b x = numToDigs (x `div` b) b ++ [x `mod` b]
+readBaseN :: [Char] -> [Char] -> (Maybe Integer)
+readBaseN c x = 
+    let nums = [elemIndex y c | y <- x ] 
+     in if all isJust nums then Just (toInteger $ foldl (\acc x -> acc + 2 * fromJust x) 0   nums)  else Nothing  
 
-digsToNum :: Integer -> [Integer] -> Integer
-digsToNum _ [] = 0
-digsToNum b (x:xs) = x * (b ^ (length xs)) + digsToNum b xs
+encodeBaseN :: [Char] -> Int-> [Char]
+encodeBaseN c i 
+  | i < clen = [c !! i] 
+  | otherwise = let index = (mod i clen)  in  encodeBaseN c (div (i - index) clen)  ++ [(c !! index)]
+    where clen = (length c)
 
-numToUni :: [Char] -> Integer -> Char
-numToUni charSet x = charSet !! fromInteger x 
-
-uniToNum :: [Char] -> Char -> Integer
-uniToNum charSet x = toInteger $ fromJust $ elemIndex x charSet 
-
-encodeBase ::  [Char] -> Integer -> [Char]
-encodeBase charSet x = map (\ y -> numToUni charSet y) $ numToDigs (toInteger $ length charSet) x 
-
-decodeBase :: [Char] -> [Char] -> Integer
-decodeBase charSet x = digsToNum  (toInteger $ length charSet) ( map (\ y -> uniToNum charSet y) x)
